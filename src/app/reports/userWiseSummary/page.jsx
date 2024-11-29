b3abcdd63b8f9468a132b0d87d3bccc7d3dccc0e
Sameer2319 <86525648+Sameer2319@users.noreply.github.com>
2024-11-29 23:50:21 +0530

    till reports //closed comp. after deployment failed pender chekcder field

---
+++ src/app/reports/userWiseSummary/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserWiseSummary = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default UserWiseSummary;
--- src/app/reports/viewEventChecklist/page.jsx
+++ src/app/reports/viewEventChecklist/page.jsx
@@ -6,4 +6,4 @@ const ViewEventChecklist = () => {
   )
 }
 
-export default ViewEventChecklist
+export default ViewEventChecklist;
